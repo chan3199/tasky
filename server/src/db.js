10f58d9e405749aa5576e0bc3b45078668a08aaa
Lee jeongchan <sjk338285@gmail.com>
2025-03-26 16:41:54 +0900

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const db = new sqlite3.Database(path.join(__dirname, 'tasky.db'), (err) => {
   if (err) return console.error('DB 연결 실패:', err.message);
-  console.log('✅ SQLite 연결 성공');
+  console.log('✅ SQLite 연결 성공 !!');
 });
 
 // 테이블 생성 (최초 실행 시)
