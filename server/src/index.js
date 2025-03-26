2bd950dedf20e71556d960273341fe86186232ce
Lee jeongchan <sjk338285@gmail.com>
2025-03-26 17:45:36 +0900

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,26 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 4000;
+require('dotenv').config();
+
 const authRoutes = require('./routes/auth');
 const todoRoutes = require('./routes/todo');
 
-const corsOption = {
+app.use(cors({
   origin: 'https://wondrous-unicorn-f48837.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
-};
-
-app.options('*', cors(corsOption));
-app.use(cors(corsOption));
+}));
 
 app.use(express.json());
-app.use('/api/todos', todoRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/todos', todoRoutes);
 
 app.get('/', (req, res) => {
   res.send('Tasky API 실행 중');
 });
 
-const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
