7ccb9c311847db941314f7626994e4922a1b7560
Lee jeongchan <sjk338285@gmail.com>
2025-03-26 17:31:51 +0900

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -5,21 +5,15 @@ const app = express();
 const authRoutes = require('./routes/auth');
 const todoRoutes = require('./routes/todo');
 
-const corsOption = {
+const corsOptions = {
   origin: 'https://wondrous-unicorn-f48837.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  optionsSuccessStatus: 200,
-}
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+};
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://wondrous-unicorn-f48837.netlify.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // preflight OPTIONS 요청 대응
 
-app.use(cors(corsOption));
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 app.use('/api/auth', authRoutes);
@@ -28,6 +22,7 @@ app.get('/', (req, res) => {
   res.send('Tasky API 실행 중');
 });
 
-app.listen(4000, () => {
-  console.log('Server running on http://localhost:4000');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
